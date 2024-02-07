4d3eca691466ffd440f6ae315d0b57ef8c102c4a
render41 <gabrielservice05@gmail.com>
2024-02-07 18:00:33 -0300

    First Commit

---
+++ app/src/main/java/com/arcastudio/foodapp_toquedacasa/activities/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.arcastudio.foodapp_toquedacasa.activities.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arcastudio.foodapp_toquedacasa.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
